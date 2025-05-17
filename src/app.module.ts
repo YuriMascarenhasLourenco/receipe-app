66595f31b0bf0b60724c6ccb804f5af676ffde73
Yuri Lourenço <lourenco99yuri@gmail.com>
2025-05-17 01:47:11 -0300

    fix: start-dev and feet: typeormModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
-import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeormModule],
+  imports: [EnvironmentConfigModule, TypeOrmModule],
   controllers: [],
   providers: [],
 })
