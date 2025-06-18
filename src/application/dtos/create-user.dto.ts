212b500d91875cafbe32bfc56e60b50a96f3b883
Yuri Lourenço <lourenco99yuri@gmail.com>
2025-06-18 17:52:40 -0300

    fix: dto validation
    used the class validator to validate dto´s properties

--- src/application/dtos/create-user.dto.ts
+++ src/application/dtos/create-user.dto.ts
@@ -1,6 +1,11 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   username: string;
+  @IsString()
   password: string;
+  @IsEmail()
   email: string;
 
   constructor(username: string, password: string, email: string) {
--- src/application/dtos/recipe.dto.ts
+++ src/application/dtos/recipe.dto.ts
@@ -1,7 +1,11 @@
+import { IsArray, IsNumber, IsString } from 'class-validator';
 import { CreateRecipeDto } from './create-recipe.dto';
 export class RecipeDto extends CreateRecipeDto {
+  @IsNumber()
   recipeId: number;
+  @IsArray()
   ingredients: string[];
+  @IsString()
   instructions: string;
   constructor(title: string, ingredients: string[], instructions: string) {
     super(title);
