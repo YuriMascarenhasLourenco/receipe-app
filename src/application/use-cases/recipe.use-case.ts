28551fe3b58da72b0d020f280ce35570d7c9ae6c
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 06:47:36 -0300

    recipe use-ace with delete, getAll, update and getById Methods

--- src/application/use-cases/recipe.use-case.ts
+++ src/application/use-cases/recipe.use-case.ts
@@ -2,14 +2,29 @@ import { Inject, Injectable } from '@nestjs/common';
 import { RecipeMapper } from '../../infrastructure/mappers/recipe.mapper';
 import { RecipeDto } from '../dtos/recipe.dto';
 import { RecipeRepository } from 'src/domain/repository/recipe.repository';
+import { UpdateRecipeDto } from '../dtos/update-recipe.dto';
+import { CreateRecipeDto } from '../dtos/create-recipe.dto';
 @Injectable()
 export class RecipeUseCase {
   constructor(
     @Inject('RecipeRepository')
     private readonly recipeService: RecipeRepository,
   ) {}
-  async createRecipe(recipe: RecipeDto): Promise<RecipeDto> {
+  async createRecipe(recipe: CreateRecipeDto): Promise<RecipeDto> {
     const recipeEntity = RecipeMapper.toOrmEntity(recipe);
     return this.recipeService.create(recipeEntity);
+  }
+  async getAllRecipes(): Promise<RecipeDto[]> {
+    return this.recipeService.findAll();
+  }
+  async deleteRecipe(id: number): Promise<void> {
+    return this.recipeService.delete(id);
+  }
+  async updateRecipe(recipe: UpdateRecipeDto): Promise<RecipeDto | null> {
+    const recipeORM = RecipeMapper.toOrmEntityUpdate(recipe);
+    return this.recipeService.update(recipeORM);
+  }
+  async getRecipeById(id: number): Promise<RecipeDto | null> {
+    return this.recipeService.findById(id);
   }
 }
