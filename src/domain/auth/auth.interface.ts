af550c5168c048bfcda5f6ca9bf088b7516898eb
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 06:56:37 -0300

    auth interface modified

--- src/domain/auth/auth.interface.ts
+++ src/domain/auth/auth.interface.ts
@@ -1,6 +1,6 @@
 import { LoginDto } from 'src/application/dtos/login-user.dto';
 import { UserDto } from 'src/application/dtos/user.dto';
-export interface IAuthService {
+export interface AuthInterface {
   validateUser(email: string, password: string): Promise<UserDto | null>;
   login(user: LoginDto): Promise<{ access_token: string }>;
 }
