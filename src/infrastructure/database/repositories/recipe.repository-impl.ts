74a19cf98beca822f3e5ce409dbe9705f0143f1e
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 07:27:55 -0300

    recipe repository with new methods

--- src/infrastructure/database/repositories/recipe.repository-impl.ts
+++ src/infrastructure/database/repositories/recipe.repository-impl.ts
@@ -1,9 +1,11 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { RecipeORMEntity } from '../typeorm/recipe.orm-entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RecipeRepository } from 'src/domain/repository/recipe.repository';
 import { i18nValidationMessage } from 'nestjs-i18n';
+import { plainToInstance } from 'class-transformer';
+import { RecipeDto } from 'src/application/dtos/recipe.dto';
 
 @Injectable()
 export class RecipeRepositoryImpl implements RecipeRepository {
@@ -17,35 +19,39 @@ export class RecipeRepositoryImpl implements RecipeRepository {
   async delete(id: number): Promise<void> {
     await this.Orm.delete(id);
   }
-  async findAll(): Promise<RecipeORMEntity[]> {
-    const recipes = await this.Orm.find();
-    if (recipes.length === 0) {
+  async findAll(): Promise<RecipeDto[]> {
+    try {
+      const recipes = await this.Orm.find();
+      if (recipes.length === 0) {
+        throw new HttpException(
+          i18nValidationMessage('validation.noRecipesFound'),
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return plainToInstance(RecipeDto, recipes);
+    } catch (error) {
       throw new HttpException(
-        i18nValidationMessage('validation.noRecipesFound'),
-        404,
+        i18nValidationMessage('validation.unableToFetchRecipes'),
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-    return recipes;
   }
   async findById(id: number): Promise<RecipeORMEntity | null> {
     const recipe = await this.Orm.findOne({ where: { id } });
     if (!recipe) {
       throw new HttpException(
         i18nValidationMessage('validation.recipeNotFound'),
-        404,
+        HttpStatus.NOT_FOUND,
       );
     }
     return recipe;
   }
-  async update(
-    id: number,
-    recipe: RecipeORMEntity,
-  ): Promise<RecipeORMEntity | null> {
-    const existingRecipe = await this.Orm.findOne({ where: { id } });
+  async update(recipe: RecipeORMEntity): Promise<RecipeORMEntity | null> {
+    const existingRecipe = await this.Orm.findOne({ where: { id: recipe.id } });
     if (!existingRecipe) {
       throw new HttpException(
         i18nValidationMessage('validation.recipeNotFound'),
-        404,
+        HttpStatus.NOT_FOUND,
       );
     }
     existingRecipe.title = recipe.title;
