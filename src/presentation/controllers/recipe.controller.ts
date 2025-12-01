930d0d20287f256321793353c3db05c7eb2500a5
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 12:20:59 -0300

    recipe controller with new routes

--- src/presentation/controllers/recipe.controller.ts
+++ src/presentation/controllers/recipe.controller.ts
@@ -1,17 +1,49 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody } from '@nestjs/swagger';
+import { CreateRecipeDto } from 'src/application/dtos/create-recipe.dto';
 import { RecipeDto } from 'src/application/dtos/recipe.dto';
+import { UpdateRecipeDto } from 'src/application/dtos/update-recipe.dto';
 import { RecipeUseCase } from 'src/application/use-cases/recipe.use-case';
 
+@ApiBearerAuth('access-token')
 @Controller('recipes')
 export class RecipeController {
   constructor(private readonly recipeUseCase: RecipeUseCase) {}
 
-  @ApiBearerAuth('access-token')
-  @UseGuards(AuthGuard('jwt'))
   @Post('/new')
-  async createRecipe(@Body() recipeDto: RecipeDto) {
-    return this.recipeUseCase.createRecipe(recipeDto);
+  async createRecipe(@Body() data: CreateRecipeDto) {
+    return this.recipeUseCase.createRecipe(data);
+  }
+  @Get()
+  async getAllRecipes(): Promise<RecipeDto[]> {
+    return this.recipeUseCase.getAllRecipes();
+  }
+  @Patch()
+  async updateRecipe(@Body() data: UpdateRecipeDto): Promise<RecipeDto | null> {
+    return this.recipeUseCase.updateRecipe(data);
+  }
+  @Delete()
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        id: { type: 'number', example: 1 },
+      },
+    },
+  })
+  async deleteRecipe(@Body('id') id: number): Promise<void> {
+    return this.recipeUseCase.deleteRecipe(id);
+  }
+  @Get('/:id')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        id: { type: 'number', example: 1 },
+      },
+    },
+  })
+  async getRecipeById(@Body('id') id: number): Promise<RecipeDto | null> {
+    return this.recipeUseCase.getRecipeById(id);
   }
 }
