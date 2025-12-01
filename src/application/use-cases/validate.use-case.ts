ecd301d5292386ae1a7211e697cf6419da0e7d57
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 06:54:41 -0300

    validate use-case modified

--- src/application/use-cases/validate.use-case.ts
+++ src/application/use-cases/validate.use-case.ts
@@ -1,15 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { IAuthService } from 'src/domain/auth/auth.interface';
+import { AuthInterface } from 'src/domain/auth/auth.interface';
 import { UserDto } from '../dtos/user.dto';
 
 @Injectable()
 export class ValidateUserUseCase {
   constructor(
-    @Inject('IAuthService') private readonly authService: IAuthService,
+    @Inject('AuthInterface') private readonly authService: AuthInterface,
   ) {}
 
   async execute(email: string, password: string): Promise<UserDto | null> {
-    console.log('email:', email, 'password:', password);
     return await this.authService.validateUser(email, password);
   }
 }
