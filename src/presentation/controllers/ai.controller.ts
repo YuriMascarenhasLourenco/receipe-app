545d53a0f56eef448a218f37130c31f4b6a2854d
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 22:32:10 -0300

    ai controller documented

--- src/presentation/controllers/ai.controller.ts
+++ src/presentation/controllers/ai.controller.ts
@@ -1,14 +1,26 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { generateRecipeDto } from 'src/application/dtos/generate-recipe.dto';
 import { GenerateTextUseCase } from 'src/application/use-cases/generate-recipe.use-case';
 import { Public } from 'src/infrastructure/auth/decorators/public.decorator';
 
+@ApiTags('AI')
 @Controller('text')
 export class TextController {
   constructor(private readonly generateTextUseCase: GenerateTextUseCase) {}
 
   @Public()
   @Post('generate')
+  @ApiOperation({
+    summary: 'Generate a recipe using AI',
+  })
+  @ApiBody({
+    schema: {
+      example: {
+        title: 'Pancakes',
+      },
+    },
+  })
   async generate(
     @Body() prompt: generateRecipeDto,
   ): Promise<{ result: string }> {
