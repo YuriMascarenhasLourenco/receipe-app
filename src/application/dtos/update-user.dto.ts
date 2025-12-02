7277db49a3526f21afbf06f9e14e1acde982f494
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 22:59:12 -0300

    update-user documented

--- src/application/dtos/update-user.dto.ts
+++ src/application/dtos/update-user.dto.ts
@@ -1,12 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class updateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the user id',
+    example: 1,
+  })
   id: number;
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the user name',
+    example: 'John Doe',
+  })
   name?: string;
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the user email',
+    example: 'anyName@domain.com',
+  })
   email?: string;
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the user password',
+    example: 'strongPassword123',
+  })
   password?: string;
 }
