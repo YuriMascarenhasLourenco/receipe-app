7031797343132a00a432d5820a35cd460d618ccb
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 22:42:39 -0300

    create-recipe dto documented

--- src/application/dtos/create-recipe.dto.ts
+++ src/application/dtos/create-recipe.dto.ts
@@ -2,13 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 import { i18nValidationMessage } from 'nestjs-i18n';
 export class CreateRecipeDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the recipe name',
+    example: 'Pancakes',
+  })
   @IsString({ message: i18nValidationMessage('validation.title') })
   title: string;
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the recipe ingredients',
+    example: 'Flour, Eggs, Milk, Sugar, Baking Powder',
+  })
   @IsString({ message: i18nValidationMessage('validation.ingredients') })
   ingredients: string;
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the recipe instructions',
+    example: '1. Mix ingredients. 2. Cook on a griddle until golden brown.',
+  })
   @IsString({ message: i18nValidationMessage('validation.instructions') })
   instructions: string;
 
