1d2abbb027bd4a03997710f4889b40865e9df591
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 22:55:24 -0300

    generate-recipe dto completed

--- src/application/dtos/generate-recipe.dto.ts
+++ src/application/dtos/generate-recipe.dto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class generateRecipeDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'the recipe name',
+    example: 'Pancakes',
+  })
   @IsString()
   title: string;
 }
