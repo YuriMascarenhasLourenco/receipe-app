4465453b3cebb79e1c69e8dd20db828bec0f465d
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-01 22:22:11 -0300

    swagger structured

--- src/main.ts
+++ src/main.ts
@@ -13,15 +13,32 @@ async function bootstrap() {
         scheme: 'bearer',
         bearerFormat: 'JWT',
         in: 'header',
+        description: 'Enter the JWT token below',
       },
       'access-token',
     )
-    .setDescription('The smart recipe book')
+    .setDescription(
+      `
+      The smart recipe book API documentation. Generate recipes from American and Brazilian cuisine and storage like your own recipe book.
+      
+      This documentation includes the following features:
+
+      **✔ JWT Authentication**  
+      ✔ User registration and profile management  
+      ✔ Recipe creation, updating, deletion, and retrieval
+      ✔ Ai-powered recipe generation from prompts (Azure AI Foundry)
+      ✔ Clean Architecture (Domain → Application → Infra → Presentation)**`,
+    )
     .setVersion('1.0')
-    .addTag('recipeAI')
+    .addTag('Auth', 'Autentication and user manegement')
+    .addTag('Recipes', 'Generation and recipe smart search')
+    .addTag('User', 'Profile and user management')
+    .addTag('AI', 'Azure AI Foundry integration')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    customSiteTitle: 'Recipe Book AI — API Docs',
+  });
 
   await app.listen(process.env.PORT);
 }
