49b5b6516ab143f5e40d4aca94d7f6bf1d1b338e
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-04 23:09:12 -0300

    adding parameter lang

--- src/application/use-cases/generate-recipe.use-case.ts
+++ src/application/use-cases/generate-recipe.use-case.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { AiServiceInterface } from 'src/domain/services/huggingface.service';
 
 import { generateRecipeDto } from '../dtos/generate-recipe.dto';
+import { Language } from 'src/domain/common/language.common';
 
 @Injectable()
 export class GenerateTextUseCase {
@@ -10,7 +11,7 @@ export class GenerateTextUseCase {
     private readonly huggingFaceService: AiServiceInterface,
   ) {}
 
-  async execute(prompt: generateRecipeDto): Promise<string> {
-    return this.huggingFaceService.chat(prompt);
+  async execute(prompt: generateRecipeDto, lang: Language): Promise<string> {
+    return this.huggingFaceService.chat(prompt, lang);
   }
 }
