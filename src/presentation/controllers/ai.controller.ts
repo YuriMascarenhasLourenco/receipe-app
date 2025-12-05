7e757d9cda8f67f52fa254ce44e328d15186bab2
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-04 23:04:26 -0300

    language header decorator and i18n integrated

--- src/presentation/controllers/ai.controller.ts
+++ src/presentation/controllers/ai.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { I18n, I18nContext } from 'nestjs-i18n';
 import { generateRecipeDto } from 'src/application/dtos/generate-recipe.dto';
 import { GenerateTextUseCase } from 'src/application/use-cases/generate-recipe.use-case';
+import { Language } from 'src/domain/common/language.common';
 import { Public } from 'src/infrastructure/auth/decorators/public.decorator';
+import { LanguageHeader } from '../decorators/language-header.decorator';
 
 @ApiTags('AI')
+@LanguageHeader()
 @Controller('text')
 export class TextController {
   constructor(private readonly generateTextUseCase: GenerateTextUseCase) {}
@@ -23,8 +27,11 @@ export class TextController {
   })
   async generate(
     @Body() prompt: generateRecipeDto,
+    @I18n() i18n: I18nContext,
   ): Promise<{ result: string }> {
-    const result = await this.generateTextUseCase.execute(prompt);
+    const lang = i18n.lang as Language;
+
+    const result = await this.generateTextUseCase.execute(prompt, lang);
     return { result };
   }
 }
