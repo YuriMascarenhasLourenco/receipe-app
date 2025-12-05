c36b6ca9d757dc145f1544011ecdbab863ade9bc
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-04 22:51:59 -0300

    langguage decorator at recipe controller

--- src/presentation/controllers/recipe.controller.ts
+++ src/presentation/controllers/recipe.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiBody,
-  ApiHeader,
   ApiOperation,
   ApiResponse,
   ApiTags,
@@ -11,12 +10,8 @@ import { CreateRecipeDto } from 'src/application/dtos/create-recipe.dto';
 import { RecipeDto } from 'src/application/dtos/recipe.dto';
 import { UpdateRecipeDto } from 'src/application/dtos/update-recipe.dto';
 import { RecipeUseCase } from 'src/application/use-cases/recipe.use-case';
-@ApiHeader({
-  name: 'Accept-Language',
-  description:
-    'Language preference for the response. Supported values: en (English), pt (Portuguese). Default is en.',
-  required: false,
-})
+import { LanguageHeader } from '../decorators/language-header.decorator';
+@LanguageHeader()
 @ApiBearerAuth('access-token')
 @Controller('recipes')
 @ApiTags('Recipes')
